41499cc3ded2794c0a93b38df8bc9770fe5de13c
Ammar <ammar.gunner1@gmail.com>
2018-11-21 23:13:13 +0100

    initial upload

---
+++ ConsoleUI/ContainerConfig.cs
@@ -0,0 +1,37 @@
+﻿using Autofac;
+using DemoLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleUI
+{
+    public static class ContainerConfig
+    {
+        // Store classes in container
+        public static IContainer Configure()
+        {
+            var builder = new ContainerBuilder();
+
+            builder.RegisterType<Application>().As<IApplication>();
+
+            //IBusinessLogic return instance of BetterBusinessLogic
+            builder.RegisterType<BetterBusinessLogic>().As<IBusinessLogic>();
+
+            //builder.RegisterType<BusinessLogic>().As<IBusinessLogic>();
+
+            //in DemoLibrary find where the namespace contains word Utilities and get interface equals I+name
+            builder.RegisterAssemblyTypes(Assembly.Load(nameof(DemoLibrary)))
+                .Where(t => t.Namespace.Contains("Utilities"))
+                .As(t => t.GetInterfaces().FirstOrDefault(i => i.Name == "I" + t.Name));
+
+
+            // Build Container
+            return builder.Build();
+
+        }
+    }
+}
---
+++ DemoLibrary/BetterBusinessLogic.cs
@@ -0,0 +1,33 @@
+﻿using DemoLibrary.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoLibrary
+{
+    public class BetterBusinessLogic : IBusinessLogic
+    {
+        ILogger _logger;
+        IDataAccess _dataAccess;
+
+        public BetterBusinessLogic(ILogger logger, IDataAccess dataAccess)
+        {
+            _logger = logger;
+            _dataAccess = dataAccess;
+
+        }
+
+        public void ProcessData()
+        {
+            Console.WriteLine("BetterBusinessLogic is loading");
+            Console.WriteLine();
+            _logger.Log("Starting the processing of data.");
+            Console.WriteLine("Processing the data");
+            _dataAccess.LoadData();
+            _dataAccess.SaveData("ProcessedInfo");
+            _logger.Log("Finished processing of the data.");
+        }
+    }
+}
